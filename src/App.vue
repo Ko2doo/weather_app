bf986dd024e5322b5f0398904a3993b848b3dcb8
Ko2doo <galaxyrobotix@gmail.com>
2026-08-31 00:16:54 +0500

    UPD:2026-08-31:: micro upd

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,47 @@ import Button from "@components/Button.vue";
 </script>
 
 <template>
-  <h1>текст</h1>
-
-  <Button>Сохранить</Button>
+  <main class="container">
+    <section class="weather-box">
+      <Button>Изменить город</Button>
+    </section>
+  </main>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+@use "@styles/tools/tools" as *;
+@use "@styles/tools/mixins" as *;
+
+.container {
+  width: 100%;
+  min-height: 100dvh;
+
+  margin-left: auto;
+  margin-right: auto;
+
+  display: flex;
+  place-items: center;
+
+  @media (min-width: rem(1200)) {
+    max-width: rem(944);
+  }
+}
+
+.weather-box {
+  width: 100%;
+
+  flex-basis: 100%;
+  display: flex;
+  flex-direction: column;
+
+  padding: rem(55) rem(50);
+  margin: 0 rem(20);
+
+  background-color: var(--secondary-color);
+  border-radius: var(--border-radius-l);
+
+  @media (min-width: rem(1200)) {
+    margin: 0;
+  }
+}
+</style>
